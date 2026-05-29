303eabf1c46d41c994ff7ede5671fbacf7f91993
Khao <khao@khaodoes.dev>
2026-05-29 19:30:12 +0700

    refactor: add minecraft version as suffix to mod version

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 base {
     archivesName = properties["archives_base_name"] as String
-    version = properties["mod_version"] as String
+    version = "${properties["mod_version"] as String}+${properties["minecraft_version"] as String}"
     group = properties["maven_group"] as String
 }
 

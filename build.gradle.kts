8373985141b4d495d79c973b2c1387cbfb50bc3c
Crosby <32882447+crosby-moe@users.noreply.github.com>
2026-07-18 22:07:11 -0400

    bump deps, update to gradle 9.6, clear gradle 10 deprecation warnings

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
-    id("fabric-loom") version "1.16-SNAPSHOT"
+    alias(libs.plugins.fabric.loom)
     id("maven-publish")
 }
 
 base {
-    archivesName = properties["archives_base_name"] as String
-    version = "${properties["mod_version"] as String}+${properties["minecraft_version"] as String}"
-    group = properties["maven_group"] as String
+    archivesName.set(providers.gradleProperty("archives_base_name"))
+    version = "${providers.gradleProperty("mod_version").get()}+${providers.gradleProperty("minecraft_version").get()}"
+    group = providers.gradleProperty("maven_group").get()
 }
 
 repositories {
@@ -16,26 +16,37 @@ repositories {
     }
 }
 
+configurations {
+    val library = create("library")
+
+    implementation.configure {
+        extendsFrom(library)
+    }
+    include.configure {
+        extendsFrom(library)
+    }
+}
+
 dependencies {
-    // To change the versions see the gradle.properties file
-    minecraft("com.mojang:minecraft:${project.properties["minecraft_version"] as String}")
-    mappings("net.fabricmc:yarn:${project.properties["yarn_mappings"] as String}:v2")
-    modImplementation("net.fabricmc:fabric-loader:${project.properties["loader_version"] as String}")
+    minecraft(libs.minecraft)
+    mappings(variantOf(libs.yarn) { classifier("v2") })
+    modImplementation(libs.fabric.loader)
+    modImplementation(libs.fabric.api)
 
-    modImplementation("net.fabricmc.fabric-api:fabric-api:${project.properties["fabric_version"] as String}")
+    modCompileOnly(libs.modmenu)
 
-    include(implementation("io.netty:netty-handler-proxy:${project.properties["netty_version"]}")!!)
-    include(implementation("io.netty:netty-codec-socks:${project.properties["netty_version"]}")!!)
+    val library = configurations.named("library")
 
-    modCompileOnly("com.terraformersmc:modmenu:${project.properties["modmenu_version"] as String}")
+    library(libs.netty.handler.proxy) { isTransitive = false }
+    library(libs.netty.codec.socks) { isTransitive = false }
 }
 
 tasks {
     processResources {
         val properties = mapOf(
             "version" to project.version,
-            "minecraft_version" to project.properties["minecraft_version"] as String,
-            "loader_version" to project.properties["loader_version"] as String
+            "minecraft_version" to libs.versions.minecraft.get(),
+            "loader_version" to libs.versions.fabric.loader.get()
         )
 
         filteringCharset = "UTF-8"
